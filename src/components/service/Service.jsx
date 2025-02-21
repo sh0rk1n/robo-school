fdcd020235d3ddbd93a8bbb6200db0930b8cb077
sh0rk1n <sh0rk1n@mail.ru>
2025-02-21 23:33:48 +0300

    package layout done

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,10 +1,36 @@
 import styles from './Service.module.scss';
 
+const packages = [
+  {
+    title: 'PRO',
+    price: '20.000 ₽',
+    description: 'УМК по робототехнике и программированию',
+  },
+  { title: 'ROBO', price: '15.000 ₽', description: 'УМК по робототехнике' },
+  { title: 'PROG', price: '10.000 ₽', description: 'УМК по программированию' },
+];
+
 export const Service = () => {
   return (
     <section className={styles.service}>
       <div className={`${styles.serviceDiv} container`}>
         <h2 className={styles.heading}>Выберите нужный пакет</h2>
+        <ul className={`${styles.list} grid`}>
+          {packages.map((pkg, index) => (
+            <li key={index} className={styles.item}>
+              <article className={styles.card}>
+                <div className={styles.content}>
+                  <h3 className={styles.title}>– {pkg.title} –</h3>
+                  <div className={styles.text}>
+                    <span className={styles.price}>{pkg.price}</span>
+                    <p className={styles.descr}>{pkg.description}</p>
+                  </div>
+                  <button className={styles.btn}>Оставить заявку</button>
+                </div>
+              </article>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
